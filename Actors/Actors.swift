0973a61efe27d0ec25e1265559eb0e1b3aec2653
Elaidzha1940 <eli080293@gmail.com>
2024-01-06 02:55:17 +0300

    Actors

---
+++ Actors/Actors.swift
@@ -0,0 +1,26 @@
+//  /*
+//
+//  Project: Actors
+//  File: Actors.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 06.01.2024
+//
+//  */
+
+import SwiftUI
+
+struct Actors: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    Actors()
+}
---
+++ Actors/ActorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActorsApp.swift
+//  Actors
+//
+//  Created by Elaidzha Shchukin on 06.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ActorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Actors()
+        }
+    }
+}
