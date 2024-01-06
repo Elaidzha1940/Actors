37246f77337f2de6b4744318ec0ee92791fba9dd
Elaidzha1940 <eli080293@gmail.com>
2024-01-06 16:02:39 +0300

    Add myDataManager

--- Actors/Actors.swift
+++ Actors/Actors.swift
@@ -9,15 +9,77 @@
 
 import SwiftUI
 
+class myDataManager {
+    static let instance = myDataManager()
+    
+    private init() { }
+    
+    var data: [String] = []
+    
+    func getRandomData() -> String? {
+        self.data.append(UUID().uuidString)
+        print(Thread.current)
+        return data.randomElement()
+    }
+}
+
+struct HomeView: View {
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.1, tolerance: nil, on: .main, in: .common, options: nil).autoconnect()
+    let manager = myDataManager.instance
+
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.7)
+                .ignoresSafeArea()
+            
+            Text(text)
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+        }
+        .onReceive(timer, perform: { _ in
+            if let data = manager.getRandomData() {
+                self.text = data
+            }
+        })
+    }
+}
+
+struct ProfileView: View {
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.1, tolerance: nil, on: .main, in: .common, options: nil).autoconnect()
+    let manager = myDataManager.instance
+    
+    var body: some View {
+        ZStack {
+            Color.brown.opacity(0.7)
+                .ignoresSafeArea()
+            
+            Text(text)
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+        }
+        .onReceive(timer, perform: { _ in
+            if let data = manager.getRandomData() {
+                self.text = data
+            }
+        })
+    }
+}
+
+
 struct Actors: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person.fill")
+                }
         }
-        .padding()
     }
 }
 
